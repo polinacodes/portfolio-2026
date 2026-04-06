08af885086dc9d03deafbf9f2938bf13c67ec7cc
PauDev1 <pa.o.liz666@gmail.com>
2026-04-06 13:44:26 -0300

    fix: image optimization and tsconfig deprecations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
-  
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop' 
+    }
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
